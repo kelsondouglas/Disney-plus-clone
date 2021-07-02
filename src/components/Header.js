3dc27484ac530330492873220759a1fe4018f34f
Kelson Douglas <kelsondouglas23@hotmail.com>
2021-07-02 14:36:00 -0300

    initializing

---
+++ src/components/Header.js
@@ -0,0 +1,69 @@
+import React from 'react';
+import styled from 'styled-components'
+
+function Header(){
+    return (
+        <Nav>
+            <Logo src='images/logo.svg' alt="Disney+" />
+            <NavMenu>
+                <a href='#'>
+                    <img src='images/home-icon.svg' alt="Home" />
+                    <span>HOME</span>
+                </a>
+                <a href='#'>
+                    <img src='images/search-icon.svg' alt="Home" />
+                    <span>SEARCH</span>
+                </a>
+                <a href='#'>
+                    <img src='images/watchlist-icon.svg' alt="Home" />
+                    <span>WATCH LIST</span>
+                </a>
+                <a href='#'>
+                    <img src='images/original-icon.svg' alt="Home" />
+                    <span>ORIGINALS</span>
+                </a>
+                <a href='#'>
+                    <img src='images/movie-icon.svg' alt="Home" />
+                    <span>MOVIES</span>
+                </a>
+                <a href='#'>
+                    <img src='images/series-icon.svg' alt="Home" />
+                    <span>SERIES</span>
+                </a>
+            </NavMenu>
+        </Nav>
+    )
+}
+
+export default Header
+
+const Nav = styled.div`
+    height: 70px;
+    background: #090b13;
+    display: flex;
+    align-items: center;
+    padding: 0 36px;
+`
+
+const Logo = styled.img`
+    width: 80px;
+`
+
+const NavMenu = styled.div`
+    display: flex;
+
+    a {
+        display: flex;
+        align-items: center;
+        padding: 0 12px;
+
+        img {
+            height: 20px;
+        }
+
+        span {
+            font-size: 13px;
+            letter-spacing: 1.42px;
+        }
+    }
+`
