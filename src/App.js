eb97a7981aa8e1acef6d050975738f2113dd81ed
Kelson Douglas <kelsondouglas23@hotmail.com>
2021-08-17 11:12:31 -0300

    Configurando Redux

--- src/App.js
+++ src/App.js
@@ -1,13 +1,28 @@
-import React from 'react';
-import './App.css';
-import Header from './components/Header'
-import Home from './components/Home';
+import React from "react";
+import "./App.css";
+import Header from "./components/Header";
+import Home from "./components/Home";
+import Detail from "./components/Detail";
+import Login from "./components/Login";
+import { BrowserRouter as Router, Switch, Route, Link } from "react-router-dom";
 
 function App() {
   return (
     <>
-    <Header />
-    <Home />
+      <Router>
+        <Header />
+        <Switch>
+          <Route path="/login">
+            <Login />
+          </Route>
+          <Route path="/detail">
+            <Detail />
+          </Route>
+          <Route path="/">
+            <Home />
+          </Route>
+        </Switch>
+      </Router>
     </>
   );
 }
